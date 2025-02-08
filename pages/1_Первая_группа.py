36c3a45760119a67abb40af1251691b4b2f84f7e
MyProjects <118967934+Osipov-01@users.noreply.github.com>
2025-02-09 00:56:30 +0300

    Update 1_Первая_группа.py

--- pages/1_Первая_группа.py
+++ pages/1_Первая_группа.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 st.image("https://teatrium.ru/upload/medialibrary/af0/2t0u03cf8i13p4o0oven20z5gasagnjl/vtb.jpg",
-         use_column_width=True)
+         use_container_width=True)
 st.title('Внутригрупповый конкурс по продажам')
 st.header('Первая группа')
 
